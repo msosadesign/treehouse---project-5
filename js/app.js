469809b418ed15dbb65b0fe9b8b4adc109d8b33c
Miguel Sosa <59977013+msosadesign@users.noreply.github.com>
2020-05-06 08:48:28 +0200

    First commit: project not yet ready

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log('Javascript Working!');
+
+$('body').prepend('<header class="searchbar wrapper"><input type="search" name="searchbar" placeholder="Search"></header>');
+
+lightbox.option({
+  'alwaysShowNavOnTouchDevices': true,
+  'showImageNumberLabel': false,
+  'disableScrolling': true,
+});
